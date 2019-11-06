c8513bfddda64cc06e88d3465a0e588374068075
ferdyandannes <fdannes@gmail.com>
2019-11-06 16:46:06 +0800

    Add files via upload

---
+++ example.R
@@ -0,0 +1,17 @@
+goog <- c(450, 451, 452, 445, 468)
+msft <- c(230, 231, 232, 233, 220)
+stocks <- c(goog, msft)
+
+stock.matrix <- matrix(stocks, byrow=T, nrow = 2)
+
+days <- c('Mon', 'Tue', 'Wed', 'Thu', 'Fri')
+st.names <- c('GOOG', 'MSFT')
+colnames(stock.matrix) <- days
+rownames(stock.matrix) <- st.names
+
+FB <- c(111,112,113,120,145)
+tech.stocks <- rbind(stock.matrix, FB)
+
+avg <- rowMeans(tech.stocks)
+tech.stocks <- cbind(tech.stocks, avg)
+print(tech.stocks)
